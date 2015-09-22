7b4b2b1f9971c98e6bad4000c08f1f3102d9b6bb
Gianluca Tiepolo <tiepolo.gian@gmail.com>
2015-09-22 12:11:29 +0200

    Fixed #5

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@
 #include <stdio.h>
 #include "TrisDb.h"
 #include "LogManager.h"
-#include "param_t.h"
+#include "cxxopts.hpp"
 #include "Shell.h"
 
-void init(param_t params);
+void init(cxxopts::Options options);
 void run();
 void shell(std::string port, std::string socket);
 
@@ -21,80 +21,84 @@ Config* conf = nullptr;
 TrisDb *db = nullptr;
 
 int main(int argc, char *argv[]) {
-    param_t params;
-    params.setPreamble("TrisDB.\n");
-    params.addFlag("--version", false, "version", "Show version number");
-    params.addFlag("-v", false, "version-compact", "Show version number");
-    params.addFlag("-h", false, "help-compact", "Show help");
-    params.addFlag("--shell", false, "shell", "Start command-line shell");
-    params.addFlag("-s", false, "shell-compact", "Start command-line shell");
-    params.addFlag("--port", "1205", "port", "Shell port");
-    params.addFlag("--socket", "nosocket", "socket", "Unix socket");
-    params.addFlag("--benchmark", false, "benchmark", "Start benchmark utility");
-    params.addFlag("-b", false, "benchmark-compact", "Start benchmark utility");
-    params.addFlag("--config", "noconfig", "config", "Specify configuration file");
-    params.addFlag("-c", "noconfig", "config-compact", "Specify configuration file");
+	try {
+		cxxopts::Options options(argv[0], " - command line options");
+		options.add_options()("c,config", "configuration file",
+				cxxopts::value<std::string>(), "FILE")("s,shell",
+				"start command-line shell")("b,benchmark",
+				"start benchmark utility")("v,version", "print version number")(
+				"h,help", "print help");
 
-    try {
-        params.parseCommandLine(argc, argv);
-        init(params);
-        run();
-    } catch (std::exception& e) {
-        LogManager::getSingleton()->log(LogManager::LERROR, e.what());
-        return 1;
-    }
+		options.add_options("Shell")("port", "Tcp port", cxxopts::value<int>(),
+				"PORT")("socket", "Unix socket", cxxopts::value<std::string>(),
+				"FILE");
 
-    delete conf;
-    LogManager::getSingleton()->log(LogManager::LINFO, "Exiting, bye bye");
-    return 0;
+		options.parse(argc, argv);
+		init(options);
+		run();
+	} catch (std::exception& e) {
+		LogManager::getSingleton()->log(LogManager::LERROR, e.what());
+		return 1;
+	}
+
+	delete conf;
+	LogManager::getSingleton()->log(LogManager::LINFO, "Exiting, bye bye");
+	return 0;
 }
 
-void init(param_t params) {
-    if (params.getBoolFlag("--version") || params.getBoolFlag("-v")) {
-        std::cout << TRISDB_VERSION_STR << std::endl;
-        exit(0);
-    } else if (params.getBoolFlag("-h")) {
-        params.printHelp();
-        exit(0);
-    } else if (params.getBoolFlag("--shell") || params.getBoolFlag("-s")) {
-        shell(params.getStringFlag("--port"), params.getStringFlag("--socket"));
-    } else if (params.getBoolFlag("--benchmark") || params.getBoolFlag("-b")) {
-        conf = new Config();
-        conf->loadDefaults();
-        db = new TrisDb(conf);
-        db->benchmark();
-        delete db;
-        delete conf;
-        exit(0);
-    }
-    // If no version, help or shell, read settings from file or use defaults and save in config var
-    if (strcmp(params.getStringFlag("--config").c_str(), "noconfig") != 0) {
-        // use specified config file
-        conf = Config::loadFromFile(params.getStringFlag("--config"));
-    } else if (strcmp(params.getStringFlag("-c").c_str(), "noconfig") != 0) {
-        conf = Config::loadFromFile(params.getStringFlag("-c"));
-    } else {
-        // no config file, use defaults
-        conf = new Config();
-        conf->loadDefaults();
-    }
+void init(cxxopts::Options options) {
+	if (options.count("help")) {
+		std::cout << options.help( { "", "Shell" }) << std::endl;
+		exit(0);
+	} else if (options.count("version")) {
+		std::cout << TRISDB_VERSION_STR << std::endl;
+		exit(0);
+	} else if (options.count("shell")) {
+		if (options.count("port")) {
+			shell(std::to_string(options["port"].as<int>()), "nosocket");
+		} else if (options.count("socket")) {
+			shell("", options["socket"].as<std::string>());
+		} else {
+			shell("1205", "nosocket");
+		}
+	} else if (options.count("benchmark")) {
+		conf = new Config();
+		conf->loadDefaults();
+		db = new TrisDb(conf);
+		db->benchmark();
+		delete db;
+		delete conf;
+		exit(0);
+	} else {
+		// If no version, help or shell, read settings from file or use defaults and save in config var
+		if (options.count("config")) {
+			conf = Config::loadFromFile(options["config"].as<std::string>());
+		} else {
+			// no config file, use defaults
+			conf = new Config();
+			conf->loadDefaults();
+		}
+	}
 }
 
 void run() {
-    Utils::printAsciiLogo();
-    LogManager::getSingleton()->log(LogManager::LINFO, TRISDB_VERSION_STR);
-    LogManager::getSingleton()->log(LogManager::LINFO, "Using config " + conf->getName());
-    LogManager::getSingleton()->log(LogManager::LINFO, "Logging to " + conf->getSetting("logfile"));
-    db = new TrisDb(conf);
-    db->run();
-    LogManager::getSingleton()->log(LogManager::LINFO, "Shutting down database");
-    delete db;
+	Utils::printAsciiLogo();
+	LogManager::getSingleton()->log(LogManager::LINFO, TRISDB_VERSION_STR);
+	LogManager::getSingleton()->log(LogManager::LINFO,
+			"Using config " + conf->getName());
+	LogManager::getSingleton()->log(LogManager::LINFO,
+			"Logging to " + conf->getSetting("logfile"));
+	db = new TrisDb(conf);
+	db->run();
+	LogManager::getSingleton()->log(LogManager::LINFO,
+			"Shutting down database");
+	delete db;
 }
 
 void shell(std::string port, std::string socket) {
-    LogManager::getSingleton()->log(LogManager::LINFO, TRISDB_VERSION_STR);
-    Shell* shell = new Shell(port, socket);
-    shell->run();
-    delete shell;
-    exit(0);
+	LogManager::getSingleton()->log(LogManager::LINFO, TRISDB_VERSION_STR);
+	Shell* shell = new Shell(port, socket);
+	shell->run();
+	delete shell;
+	exit(0);
 }
